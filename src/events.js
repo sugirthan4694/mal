57025d4e3490ee1d8cd46862c8ba0c65a848179e
Sugirthan Ramachandran <sugirthan.ramachandr@workday.com>
2026-08-24 12:59:03 +0530

    events

---
+++ src/events.js
@@ -0,0 +1,18 @@
+const ACCOUNTS = {
+  'ACC-001': { currency: 'AED' },
+  'ACC-002': { currency: 'BHD' },
+};
+
+const EVENTS = [
+  { id: 'E1', day: 1, type: 'CREDIT', account: 'ACC-001', amount: '1200.00', valueDay: 1 },
+  { id: 'E2', day: 1, type: 'DEBIT', account: 'ACC-001', amount: '950.00', valueDay: 1 },
+  { id: 'E3', day: 2, type: 'AUTHORIZATION', account: 'ACC-001', authId: 'Auth-A', amount: '200.00', valueDay: 2 },
+  { id: 'E4', day: 3, type: 'CREDIT', account: 'ACC-001', amount: '400.00', valueDay: 3 },
+  { id: 'E5', day: 4, type: 'SETTLEMENT', account: 'ACC-001', authId: 'Auth-A', amount: '185.00', valueDay: 4 },
+  { id: 'E6', day: 5, type: 'DEBIT', account: 'ACC-001', amount: '620.00', valueDay: 2 },
+  { id: 'E7', day: 5, type: 'AUTHORIZATION', account: 'ACC-001', authId: 'Auth-B', amount: '90.00', valueDay: 5 },
+  { id: 'E8', day: 6, type: 'REVERSAL', account: 'ACC-001', reverses: 'E7', valueDay: 2 },
+  { id: 'E9', day: 5, type: 'CREDIT', account: 'ACC-002', amount: '10.000', valueDay: 5, instalments: 3 },
+];
+
+module.exports = { ACCOUNTS, EVENTS };
---
+++ src/ledger.js
@@ -0,0 +1,47 @@
+const { toMinor, formatMinor } = require('./money');
+
+class Account {
+  constructor(id, currency, openingBalance = '0') {
+    this.id = id;
+    this.currency = currency;
+    this.postings = [];
+    this.holds = new Map();
+    this.holdEvents = [];
+    this.dailyAccrual = new Map();
+    this.closedDays = new Map();
+    this.recomputeLog = [];
+
+    const opening = toMinor(openingBalance, currency);
+    if (opening > 0) {
+      this.postings.push({ type: 'OPENING', amountMinor: opening, valueDay: 1, bookedDay: 0, ref: 'opening' });
+    }
+  }
+
+  post(type, amountMinor, valueDay, bookedDay, ref) {
+    const posting = { type, amountMinor, valueDay, bookedDay, ref };
+    this.postings.push(posting);
+    return posting;
+  }
+
+  ledgerBalanceAsOf(day) {
+    return this.postings.reduce((sum, p) => (p.valueDay <= day ? sum + p.amountMinor : sum), 0);
+  }
+
+  activeHoldsTotal() {
+    let total = 0;
+    for (const hold of this.holds.values()) {
+      if (hold.status !== 'DECLINED') total += hold.amountMinor;
+    }
+    return total;
+  }
+
+  availableBalanceAsOf(day) {
+    return this.ledgerBalanceAsOf(day) + this.activeHoldsTotal();
+  }
+
+  format(minor) {
+    return formatMinor(minor, this.currency);
+  }
+}
+
+module.exports = { Account };
